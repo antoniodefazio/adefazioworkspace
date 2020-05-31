7225cf79417abfa28a7b6f377d0aef22f5e2fc34
Antonio Spa <antonio.defazio@virgilio.it>
2020-05-31 17:33:02 +0200

    organize imports

--- src/main/java/defazio/tutorials/demo/dto/EagerLibroDto.java
+++ src/main/java/defazio/tutorials/demo/dto/EagerLibroDto.java
@@ -1,9 +1,7 @@
 package defazio.tutorials.demo.dto;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 
 public class EagerLibroDto extends LibroDto {
